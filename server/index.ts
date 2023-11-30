038ea8ca1754388558229e9ffbd42f4493361f70
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-11-30 09:24:47 -0500

    sqlite, storing results

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,23 @@ import dotenv from 'dotenv';
 import hiscores, { getStats } from 'osrs-json-hiscores';
 dotenv.config();
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data.db');
+
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS league_points (id INTEGER PRIMARY KEY AUTOINCREMENT, rsn TEXT, date TEXT, points INTEGER, rank INTEGER)");
+})
+
 const app: Express = express();
 const port = process.env.PORT;
+
+interface LeaguePointRecord {
+  id: number;
+  rsn: string;
+  date: string;
+  points: number;
+  rank: number;
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
@@ -21,24 +36,53 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/leaguePoints/:rsn", (req, res) => {
-    console.log("Received request" + req.params.rsn)
+
+
+app.get("/leaguePoints/:rsn", (req: Request, res: Response) => {
+    console.log("Received request " + req.params.rsn)
+    const formattedDate = new Date().toISOString();
+
   hiscores
     .getStatsByGamemode(req.params.rsn, 'seasonal')
     .then((response) =>  {
-      console.log(response)
-      var leagueStats  = {
+      var leagueStats = {
         skills: response.skills,
-        points: response.leaguePoints,
+        points: response.leaguePoints.score,
+        rank: response.leaguePoints.rank,
         bosses: response.bosses
       }
-      res.send(JSON.stringify(leagueStats));})
+
+
+      db.get("SELECT * FROM league_points WHERE rsn = ? AND date = ? AND points = ? AND rank = ?", [req.params.rsn, formattedDate, leagueStats.points, leagueStats.rank], (err: { message: string; }, row: any) => {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+     
+        if (row) {
+          console.log("duplicate!")
+          res.status(400).send('Duplicate data. Already exists in the database.');
+          return;
+        }
+
+        db.run("INSERT INTO league_points (rsn, date, points, rank) VALUES (?, ?, ?, ?)", [req.params.rsn, formattedDate, leagueStats.points, leagueStats.rank], (err: { message: string; }) => {
+          if (err) {
+            console.error(err.message);
+            res.status(500).send('Internal Server Error');
+            return;
+          }
+
+          res.send(JSON.stringify(leagueStats));
+        });
+      })
+    })
     .catch((err) => {
       res.status(404).send({ status: 404, error: err });
     });
 });
 
-app.get("/stats/:rsn", (req, res) => {
+app.get("/stats/:rsn", (req: Request, res: Response) => {
      console.log("Received request" + req.params.rsn)
   hiscores
     .getStats(req.params.rsn)
@@ -46,6 +90,17 @@ app.get("/stats/:rsn", (req, res) => {
     .catch((err) => {
       res.status(404).send({ status: 404, error: err });
     });
+});
+
+process.on('SIGINT', () => {
+  db.close((err: { message: string; }) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Closed the database connection.');
+      process.exit(0);
+    }
+  });
 });
 
 app.listen(port, () => {
